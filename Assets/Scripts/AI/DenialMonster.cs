e126419c76daa23041872939a8e956d91d739831
concubicycle <saveliyb@gmail.com>
2016-01-30 22:07:11 -0500

    Enemy work

--- Assets/Scripts/AI/DenialMonster.cs
+++ Assets/Scripts/AI/DenialMonster.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Rose;
+using System.Collections;
 using UnityEngine;
 
 namespace Assets.Scripts.AI
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class DenialMonster : MonoBehaviour
     {
         public enum DenialMonsterState
@@ -13,6 +15,21 @@ namespace Assets.Scripts.AI
             Stalking,
             Showing
         }
+
+
+        #region Editor Variables
+        public ParticleSystem particleSystem;
+        public Animator animator;
+        public float stalkDistance = 7.0f;
+        public float stalkDIstanceVariance = 3.0f;
+        public float moveSpeed = 5.0f;
+        public GameObject attackCone;
+        public float attackTime = 3.0f;
+        #endregion
+
+
+        private RoseController _rose;
+        private Rigidbody2D _rigidBody2D;
 
         #region Properties
         private DenialMonsterState _state;
@@ -46,36 +63,100 @@ namespace Assets.Scripts.AI
         #endregion
 
 
-        private void start()
+        private void Awake()
         {
-            State = DenialMonsterState.Hiding;
+            _rose = FindObjectOfType<RoseController>();
+            _rigidBody2D = GetComponent<Rigidbody2D>();
         }
 
+        private void Start()
+        {
+            State = DenialMonsterState.Stalking;
+        }
+
+              
 
         #region Coroutines
         IEnumerator Attack()
         {
-            return null;
+            _rigidBody2D.velocity = Vector2.zero;
+            attackCone.SetActive(true);
+
+            attackCone.transform.MatchUpVector((_rose.transform.position - transform.position).normalized);
+            yield return new WaitForSeconds(attackTime);
+            attackCone.SetActive(false);
+
+            State = DenialMonsterState.Repositioning;
         }
 
         IEnumerator Reposition()
         {
-            return null;
+            float x = Random.Range(-1, 1);
+            Vector2 ran = new Vector2(x, 1.0f);
+
+            var mag = stalkDIstanceVariance + Random.Range(-stalkDIstanceVariance / 2, stalkDIstanceVariance / 2); //TODO: optimize
+            float startTime = Time.time;
+            float lastTime = startTime;
+            float timeElapsed = 0;
+
+            while (timeElapsed < 2.0f && Vector2.Distance(transform.position, _rose.transform.position) > mag + 5.0f)
+            {
+                float dt = Time.time - lastTime;
+                timeElapsed += dt;
+                lastTime = Time.time; 
+
+
+                Vector2 targ = (Vector2)_rose.transform.position + ran * mag;
+                Vector2 vel = (targ - (Vector2)transform.position).normalized * moveSpeed;
+
+                _rigidBody2D.velocity = vel;
+
+                yield return null;
+            }
+
+            State = DenialMonsterState.Attacking;
         }
 
         IEnumerator Hide()
         {
-            return null;
+            // play fadeout animation
+            animator.SetBool("Visible", false);
+            yield return new WaitForSeconds(1.25f);
+            particleSystem.gameObject.SetActive(true);
+            particleSystem.Stop(true);
+            State = DenialMonsterState.Stalking;
         }
 
         IEnumerator Stalk()
         {
-            return null;
+            float x = Random.Range(-1, 1);
+            Vector2 ran = new Vector2(x, 1.0f);
+
+            var mag = stalkDIstanceVariance + Random.Range(-stalkDIstanceVariance / 2, stalkDIstanceVariance / 2); //TODO: optimize
+
+            while (Vector2.Distance(transform.position, _rose.transform.position) > mag + 5.0f)
+            {                
+                Vector2 targ = (Vector2)_rose.transform.position + ran * mag;
+                Vector2 vel = (targ - (Vector2)transform.position).normalized * moveSpeed;
+
+                Debug.DrawLine(transform.position, targ, Color.red);
+
+
+                _rigidBody2D.velocity = vel;
+
+                yield return null;                
+            }
+
+            State = DenialMonsterState.Showing;
         }
 
         IEnumerator Show()
         {
-            return null;
+            animator.SetBool("Visible", true);
+            yield return new WaitForSeconds(1.25f);
+            particleSystem.gameObject.SetActive(true);
+            particleSystem.Play(true);
+            State = DenialMonsterState.Attacking;
         }
         #endregion
     }
---
+++ Assets/Scripts/AttackCone.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+namespace Assets.Scripts
+{
+    public class AttackCone : MonoBehaviour
+    {
+        public float damage = 4;
+        public ParticleSystem particleSystem;
+
+        private float _timeOfLastDamage = 0;
+        private float _cooldown = 0.4f;
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == 8)
+            {
+                if (Time.time - _timeOfLastDamage > _cooldown)
+                {
+                    other.gameObject.GetComponent<Health>().HealthPoints -= damage;
+                    _timeOfLastDamage = Time.time;
+                }
+            }
+        }
+
+        public void OnEnable()
+        {
+            particleSystem.Play(true);  
+        }
+
+        public void OnDisable()
+        {
+            particleSystem.Stop(true);
+        }
+    }
+}
