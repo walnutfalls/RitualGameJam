91c3edd9cce142ef51d38551097ca60e1dc6ec14
concubicycle <saveliyb@gmail.com>
2016-01-30 20:05:49 -0500

    Ignoring files.

---
+++ Assets/Scripts/AI/DenialMonster.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.AI
+{
+    public class DenialMonster : MonoBehaviour
+    {
+        public enum DenialMonsterState
+        {
+            Attacking,
+            Repositioning,
+            Hiding,
+            Stalking,
+            Showing
+        }
+
+        #region Properties
+        private DenialMonsterState _state;
+        public DenialMonsterState State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                switch (_state)
+                {
+                    case DenialMonsterState.Attacking:
+                        StartCoroutine(Attack());
+                        break;
+                    case DenialMonsterState.Repositioning:
+                        StartCoroutine(Reposition());
+                        break;
+                    case DenialMonsterState.Hiding:
+                        StartCoroutine(Hide());
+                        break;
+                    case DenialMonsterState.Stalking:
+                        StartCoroutine(Stalk());
+                        break;
+                    case DenialMonsterState.Showing:
+                        StartCoroutine(Show());
+                        break;
+                    default: break;
+                }
+            }
+        }
+        #endregion
+
+
+        private void start()
+        {
+            State = DenialMonsterState.Hiding;
+        }
+
+
+        #region Coroutines
+        IEnumerator Attack()
+        {
+            return null;
+        }
+
+        IEnumerator Reposition()
+        {
+            return null;
+        }
+
+        IEnumerator Hide()
+        {
+            return null;
+        }
+
+        IEnumerator Stalk()
+        {
+            return null;
+        }
+
+        IEnumerator Show()
+        {
+            return null;
+        }
+        #endregion
+    }
+}
